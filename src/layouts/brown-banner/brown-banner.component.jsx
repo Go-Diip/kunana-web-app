a01d66e62cdeed39e9c846af88e6d07ebb8a1bab
Alex Ron <bryan.ron@hotmail.com>
2022-08-22 14:53:18 -0500

    feat: init layout

---
+++ src/layouts/brown-banner/brown-banner.component.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import * as S from "./brown-banner.styles"
+
+const BrownBanner = () => {
+  return <div></div>
+}
+
+export default BrownBanner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import HomeHero from "../layouts/HomeHero";
+import HomeHero from "../layouts/HomeHero"
+import BrownBanner from "../layouts/brown-banner"
 
 const NotFoundPage = () => (
-    <Layout seo={{ title: "Kunana - Coming Soon!" }}>
-        <HomeHero />
-    </Layout>
+  <Layout seo={{ title: "Kunana - Coming Soon!" }}>
+    <HomeHero />
+    <BrownBanner />
+  </Layout>
 )
 
 export default NotFoundPage
