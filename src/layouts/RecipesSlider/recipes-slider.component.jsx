2932852f438b7b16d2fc7f6a6540eefb96fe01d8
Bryan Ron <bryan.ron@hotmail.com>
2022-08-23 17:06:04 -0500

    fix: responsive design

--- src/layouts/RecipesSlider/recipes-slider.component.jsx
+++ src/layouts/RecipesSlider/recipes-slider.component.jsx
@@ -1,68 +1,56 @@
 import React from "react"
-import { Grid } from "@mui/material"
+import { Grid, useMediaQuery, useTheme } from "@mui/material"
+import { Swiper, SwiperSlide } from "swiper/react"
 
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-
+import "swiper/css"
+import "swiper/css/navigation"
 import * as S from "./recipes-slider.styles.jsx"
-import CustomImage from "../../components/custom-image/custom-image.component"
 
 const RecipesSlider = ({ title, recipes }) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  }
+  const theme = useTheme()
+  const isSM = useMediaQuery(theme.breakpoints.down("sm"))
 
   if (!recipes) return null
-  console.log(recipes)
 
   return (
     <S.Wrapper>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
         <S.ContentGrid item xs={12} md={5}>
           <S.Title>{title}</S.Title>
-          <S.Button>view recipes</S.Button>
+          {!isSM && <S.Button>view recipes</S.Button>}
         </S.ContentGrid>
         <Grid item xs={12} md={7}>
-          <S.CustomSlider {...settings}>
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              600: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+              900: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+            }}
+            loop={true}
+          >
             {recipes.map(({ image }, index) => (
-              <S.SlideItem key={`slider-item-${index}`}>
+              <SwiperSlide key={`slider-item-${index}`}>
                 <S.SlideImage img={image} arPaddingPercentage={114} />
-              </S.SlideItem>
+              </SwiperSlide>
             ))}
-          </S.CustomSlider>
+          </Swiper>
         </Grid>
+        {isSM && (
+          <S.ButtonGrid item xs={12}>
+            <S.Button>view recipes</S.Button>
+          </S.ButtonGrid>
+        )}
       </Grid>
     </S.Wrapper>
   )
